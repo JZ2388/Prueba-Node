4426ff2d3b3005ee0354173cbcd869638ee18c79
unknown <javier.e.zamorano@gmail.com>
2022-05-30 23:40:28 -0300

    Ya estan listos los Commit

---
+++ pruebas.js
@@ -0,0 +1,76 @@
+// let dia = 'jueves'
+// function finDeSemana (dia) {
+
+// }
+
+// switch (dia) {	
+// 	case 'viernes':
+//     	console.log('buen finde')
+// 	break;
+	
+// 	case 'lunes':
+//     	console.log('buena semana')
+// 	break;
+	
+// 	default:
+//     	console.log('buen dia')
+// 	}
+
+// let dia = ("sabado");
+// 	switch (dia) {
+//     	case "lunes":
+// 		case "miercoles":
+// 		case "viernes":
+// 		console.log("tenés clases");
+// 		break
+
+// 		default: 
+// 		console.log("no tenés clases")
+	
+// }
+
+// function loro () {
+//     for (let i = 0; i < 5; i++){
+//         console.log("Holaaaaaa"); }
+//     }
+
+
+// let dia = 'viernes'
+// function finDeSemana (dia) {
+// }
+
+// switch (dia){ 	
+// 	case 'viernes':
+//     	console.log('buen finde')
+// 	break;
+	
+// 	case 'lunes':
+//     	console.log('buena semana')
+// 	break;
+	
+// 	default:
+//     	console.log('buen dia')
+// 	}
+
+
+// function esPar (n) {
+//     return n % 2 == 0 
+// }
+
+let anterior = function(numeroA){
+    return numeroA - 1 
+}
+
+let triple = function(numeroB){
+    return numeroB * 3 
+}
+
+function anteriorDelTriple (numeroC){
+    return numeroC == triple == anterior
+
+}
+
+
+console.log(triple (5));
+console.log (anterior(10))
+console.log(anteriorDelTriple(20));
